f2a490f69d24e6c1048cbfe7ddc62bd11ae1cb20
EIPP <pope090902hmcscra8@lake.edu.mx>
2025-04-09 11:17:10 -0600

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Rotomdex</h1>
+      <h2>Welcome to the rotomdex</h2>
+      <p>Here you can find information of all the pokemons</p>
+      
+      <div>
+        <p>Developed by eipp</p>
+        <p>2025</p>
+        <p>Al, rights reserved</p>
+      </div>
+    </>
+  )
+}
+
+export default App
