1c4c03c545fa4399ab69729d57b8e6dc06dd39c5
EIPP <pope090902hmcscra8@lake.edu.mx>
2025-05-08 09:20:20 -0600

    aplicacion mostrada

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
 import './App.css'
-
+import {useState, useEffect} from "react"
 function App() {
+  const [pokemones, setPokemones] = useState([])
+  useEffect(() => {
+    const fetchPokemones = async () => {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
+      const data = await response.json()
+      const { results } = data
+      
+    const detallesPokemon = await Promise.all(
+      results.map(async (pokemon)=>{
+        const respuesta = await  fetch(pokemon.url)
+        const datos = await respuesta.json()
+        return{
+          id:datos.id,
+          nombre:datos.name,
+          imagen:datos.sprites.front_default,
+          altura:datos.height,
+          peso:datos.weight,
+        }
+      })
+    )
+  setPokemones(detallesPokemon)
+  }
+    fetchPokemones()
+  }, [])
 
   return (
     <>
@@ -8,7 +32,14 @@ function App() {
       <h2>Welcome to the rotomdex</h2>
       <h3>pokegod</h3>
       <p>Here you can find information of all the pokemons</p>
-      <img src="" alt="" />
+      {pokemones.map(pokemon =>(
+      <div className='tutifruti' key={pokemon.id}>
+        <h2>{pokemon.nombre} ({pokemon.id})</h2>
+        <img src={pokemon.imagen} alt="" />
+        <p>altura: {pokemon.altura/10}m</p>
+        <p>{pokemon.peso/10}kg</p>
+      </div>
+    ))}
       <div>
         <p>Developed by E.I.P.P</p>
         <p>2025</p>
